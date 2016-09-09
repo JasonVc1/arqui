df15988aeb7cc1d7ed99f87d0703e7dd1e93f77a
JasonVc1 <jasontex_21@hotmail.com>
2016-09-09 13:12:55 -0300

    Update index.js

--- hola/controllers/index.js
+++ hola/controllers/index.js
@@ -13,5 +13,8 @@ module.exports = function (router) {
         
     });
     
-    router.post('/', function (req, res));
+    router.post('/', function (req, res){
+            var x = req.body;
+            res.send()'<code><pre>' + req.body + '</pre></code>';
+    });
 };
